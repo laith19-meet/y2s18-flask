d6eebfabc7e605a6a2cc9b26680b473200146d69
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 11:23:20 +0300

    finisheeed

--- exercises/app.py
+++ exercises/app.py
@@ -6,7 +6,9 @@ def home_page():
     players = ["cristisno ronaldo" , "messi" , "Iker casillas"]
     return render_template(
         "index.html", 
-        players = players)
+        players = players,
+        likes_same_sports = False)
+
 
 if __name__ == '__main__':
    app.run(debug = True)
